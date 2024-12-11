f7308fd21ac98ed4765e6f2329a93adb2da8da72
MeghaSRam39 <meghasram52@gmail.com>
2024-12-11 10:15:23 +0530

    added env

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
-import streamlit as st
-import pandas as pd
-import base64, random
-import time, datetime
-import io, random
-from streamlit_tags import st_tags
-from PIL import Image
-import pymysql 
-import pafy
-import plotly.express as px 
-import nltk
-nltk.download('stopwords')
+# import streamlit as st
+# import pandas as pd
+# import base64, random
+# import time, datetime
+# import io, random
+# from streamlit_tags import st_tags
+# from PIL import Image
+# import pymysql 
+import os
+# import pafy
+# import plotly.express as px 
+# import nltk
+from dotenv import load_dotenv
 
-def fetch_yt_video(link):
-    video = pafy.new(link)
-    return video.title
+load_dotenv('.env')
+
+
+password = os.getenv('password')
+print(password)
+
+# nltk.download('stopwords')
+
+# def fetch_yt_video(link):
+#     video = pafy.new(link)
+#     return video.title
 
 
 #connect to mysql db
-connection = pymysql.connect(host = 'localhost', user = 'root', password = 'meghasram52@', db = 'cv')
+connection = pymysql.connect(host = 'localhost', user = 'root', password = password, db = 'cv')
 cursor = connection.cursor()
 
